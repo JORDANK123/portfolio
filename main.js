68abc417c95038a26f54b88cf3a31e63660a34c7
Jordan K <jkulc167@mtroyal.ca>
2024-12-26 16:43:27 -0700

    Update main.js
    progress

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    let title = document.createElement('h1');
-    title.textContent = "Welcome !"
-    document.body.appendChild(title);
-    generatediv1();
-    generatediv2();
- 
- })
- 
- 
- 
- 
- 
- 
- 
- function generatediv1(){
-     let div1 = document.createElement('div');
-     div1.style.backgroundColor = "black";
-     div1.style.width = "100px";
-     document.body.appendChild(div1);
- }
- function generatediv2(){
-     let div2 = document.createElement('div');
-     div2.style.backgroundColor = "red";
-     div2.style.width = "100px";
-     document.body.appendChild(div2);
- }
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+document.addEventListener('DOMContentLoaded', () => {
+    generateHeader();
+    generateContainer();
+})
+
+function generateContainer() {
+    let container = document.createElement('div');
+    container.style.display = "flex";
+    container.style.width = "100vw";
+    container.style.height = "90vh";
+    document.body.appendChild(container);
+    generatediv1(container);
+    generatediv2(container);
+}
+
+function generatediv1(container) {
+    let div1 = document.createElement('div');
+    div1.style.backgroundColor = "black";
+    div1.style.width = "30%";
+    div1.style.height = "100%";
+    container.appendChild(div1);
+}
+
+function generatediv2(container) {
+    let div2 = document.createElement('div');
+    div2.style.backgroundColor = "red";
+    div2.style.width = "70%";
+    div2.style.height = "100%";
+    container.appendChild(div2);
+}
+function generateHeader() {
+    let header = document.createElement('header');
+    header.textContent = "Welcome !";
+    header.style.fontSize = "30px";
+    header.style.display = "flex";
+    header.style.alignItems = "center";
+    header.style.justifyContent = "center";
+    header.style.height = "10vh";
+    document.body.appendChild(header);
+}
